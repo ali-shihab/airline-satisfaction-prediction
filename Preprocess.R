1720be8886df0af89d6054e70d4760c86c1322e2
ali-shihab <ali-shihab@outlook.com>
2023-12-29 19:28:13 +0000

    all done. converted to Rmarkdown (.Rmd) from .ipynb).

--- Preprocess.R
+++ Preprocess.R
@@ -3,10 +3,10 @@
 # Ali - Group 25 - Data Pre-procesisng script.
 #
 # This script contains the custom helper functions
-# necessary for my associated ExploratoryDataAnalysis.R
+# necessary for my associated Exploratory Data Analysis
 # script. This was also assisted by the following
 # functions from the PBA lab3DataPreprocessing.R script:
-# - N
+# - NPREPROCESSING_removePunctuation()
 # ************************************************
 # ************************************************
 # Load necessary libraries
@@ -128,33 +128,3 @@ visualiseHist<-function(dataFrame){
   
   return(histPlots)
 }
-
-
-
-# One-hot encoding categorical features
-#combined <- dummyVars("~ .", data = combined) %>% predict(combined)
-
-# Standardize the data with z-score
-#combined_scaled <- scale(combined[, sapply(combined, is.numeric)])
-
-# Local Outlier Factor (LOF) Analysis
-#lof <- lofactor(combined_scaled)
-#outliers <- which(lof > 1.5) # Assuming threshold for outliers
-#combined_cleaned <- combined[-outliers,]
-
-# Random Forest for further outlier detection and removal
-#rf_model <- randomForest(satisfaction ~ ., data=combined_cleaned)
-#importance <- importance(rf_model)
-#varImpPlot(rf_model)
-
-# Stratified train-test split
-#set.seed(123)
-#split <- createDataPartition(combined_cleaned$satisfaction, p = 0.8, list = FALSE)
-#train_set <- combined_cleaned[split,]
-#test_set <- combined_cleaned[-split,]
-
-# Setting up stratified k-fold cross-validation on training data
-#folds <- createFolds(train_set$satisfaction, k = 10)
-
-# Confirming binary classification
-#unique(train_set$satisfaction)
